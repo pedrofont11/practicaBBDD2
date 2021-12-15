893f63694e7229b423f75c254d9392e8ab35dae4
Julianh1805 <husson.j@yahoo.com>
2021-12-15 21:49:56 +0100

    home routes

--- pagines/home.php
+++ pagines/home.php
@@ -18,15 +18,15 @@
             border-radius: 10px;
             margin: 10px 0;
             text-align: center;
+                width: 100%;
+                height: 40px;
+                font-size: 16px;
+                font-family: Avenir, sans-serif;
+ font-weight: 400;
             }
 
             .option:hover{
             cursor: pointer;
-            }
-
-            .title-card{
-            font-size: 14px;
-            font-weight: lighter;
             }
         </style>
     </head>
@@ -37,22 +37,25 @@
          $nomUsuari = $_GET['nomUsuari'];  
          ?>
         <h1><?php echo $nomUsuari?></h1>
-        <section class="option contracte">
-            <div class="title-card">
-                <h2>Ver contracte</h2>
-            </div>
-        </section>
-        <section class="option nou-contracte">
-            <div class="title-card">
-                <h2 onclick="<?php echo 'text' ?>">Nou contracte</h2>
-                        
-            </div>
-        </section>
-        <section class="option factures">
-            <div class="title-card">
-                <h2>Ver factures</h2>
-            </div>
-        </section>
+        <form method="post">
+            <input type="submit" name="button1"
+                   class="option" value="Nou contract" />
+            <input type="submit" name="button2"
+                   class="option" value="Veure contracte" />
+            <input type="submit" name="button2"
+                   class="option" value="Veure factura" />
+        </form>
+        <?php
+        if(isset($_POST['button1'])){
+            header("Location: ../FormulariContracte.php");
+        }
+        if(isset($_POST['button2'])){
+            header("Location: ../InsereixUsuaris.php");
+        }
+        if(isset($_POST['button3'])){
+            header("Location: home.php?nomUsuari=".$_POST['usuari']);
+        }
+        ?>
     </div>
 </div>
     </body>
